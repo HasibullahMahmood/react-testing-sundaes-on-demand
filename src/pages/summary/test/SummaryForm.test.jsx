50cb77a5d39a57f4323b667bd576f5323226c4af
Hasibullah Mahmood <hasibullahmahmood@gmail.com>
2021-12-13 21:26:02 +0300

    mock service worker setupped

--- src/pages/summary/test/SummaryForm.test.jsx
+++ src/pages/summary/test/SummaryForm.test.jsx
@@ -1,4 +1,4 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, waitForElementToBeRemoved } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import SummaryForm from '../SummaryForm';
 
@@ -25,4 +25,21 @@ test('Checkbox enables button on first click and disables on second click', () =
 
 	userEvent.click(checkbox);
 	expect(confirmButton).toBeDisabled();
+});
+
+test('Terms and Conditions Popover works', async () => {
+	render(<SummaryForm />);
+
+	// initially the popover should not be in the document
+	const nullPopover = screen.queryByText(/No ice cream will actually be delivered/i);
+	expect(nullPopover).not.toBeInTheDocument();
+
+	// hover and popover should be appeared
+	const tc = screen.getByText(/terms and conditions/i);
+	userEvent.hover(tc);
+	expect(screen.getByText(/No ice cream will actually be delivered/i)).toBeInTheDocument();
+
+	// un hover  and popover should be disappeared
+	userEvent.unhover(tc);
+	await waitForElementToBeRemoved(() => screen.queryByText(/No ice cream will actually be delivered/));
 });
