8a195b4b5d9fd65dfefc7f43b81e6959b1d4d1e6
Hasibullah Mahmood <hasibullahmahmood@gmail.com>
2021-12-14 21:51:51 +0300

    error catch testing implemented

--- src/pages/entry/Options.jsx
+++ src/pages/entry/Options.jsx
@@ -1,11 +1,14 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import Row from 'react-bootstrap/Row';
+
 import ScoopOption from './ScoopOption';
 import ToppingOption from './ToppingOption';
+import AlertBanner from '../../components/alert-banner/AlertBanner';
 
 export default function Options({ optionType }) {
 	const [items, setItems] = useState([]);
+	const [error, setError] = useState(false);
 
 	useEffect(() => {
 		(async () => {
@@ -14,13 +17,15 @@ export default function Options({ optionType }) {
 				const response = await axios.get(`http://localhost:3030/${optionType}`);
 				setItems(response.data);
 			} catch (error) {
-				// TODO: handle error
+				setError(true);
 			}
 		})();
 	}, [optionType]);
 
+	if (error) {
+		return <AlertBanner />;
+	}
 	const ElementItem = optionType === 'scoops' ? ScoopOption : ToppingOption;
-
 	const OptionItems = items.map((i) => <ElementItem key={i.name} name={i.name} imagePath={i.imagePath} />);
 
 	return <Row>{OptionItems}</Row>;
